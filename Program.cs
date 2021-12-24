f4ddbfe0b618194259c36b7b4509da7e423ee4a2
Putseyeu <putseyeu.a.d@gmail.com>
2021-12-24 19:27:47 +0100

    Step1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace localNumbersInTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[30];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            if (array[0] > array[1])
+            {
+                Console.WriteLine("Вывод локального числа " + array[0]);
+            }
+
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                int j = i + 1;
+                if (array[j] > array[i])
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Console.WriteLine("Вывод локального числа " + array[j]);
+                    }
+                }
+            }            
+            
+            if (array[array.Length - 1] > array[array.Length - 2])
+            {
+                Console.WriteLine("Вывод локального числа " + array[array.Length - 1]);
+            }               
+        }
+    }
+}
